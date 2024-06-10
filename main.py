aecce95060d6d59666065a1535c0aa7ffb672445
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-10 11:47:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     os.makedirs(shared_dir, exist_ok=True)
 
     # merge Xresources to show the correct windows titles on xterm
-    merge_xresources(script_dir)
+    # merge_xresources(script_dir)
 
     # set the logging level
     setLogLevel('info')
