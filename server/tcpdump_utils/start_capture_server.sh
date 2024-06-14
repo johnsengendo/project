18ee3f72655a0f1b00122e797a7612240f321323
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-15 00:19:37 +0200

    Update start_capture_server.sh

--- server/tcpdump_utils/start_capture_server.sh
+++ server/tcpdump_utils/start_capture_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start tcpdump in background in order to capture the video streaming traffic outgoing from the server
-tcpdump -U -s0 -i server-eth0 src port 1940 -w shared/server_out.pcap &
+tcpdump -U -s0 -i server-eth0 src port 1935 -w shared/server_out.pcap &
