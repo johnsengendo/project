377719a0c2793853e58d729eba3f65d89203295c
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-14 22:57:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
         #spawnXtermDocker('streaming_server')
         #spawnXtermDocker('streaming_client')
         #subprocess.run(['docker', 'exec', '-it', 'streaming_server', 'bash'])
-        subprocess.run(['docker', 'exec', '-it', 'streaming_server', 'bash', '-c', 'cd /home && ./stream_video.sh'])
+        subprocess.run(['docker', 'exec', '-it', 'streaming_server', 'bash', '-c', 'cd /home && ./combined.sh'])
 
         # let the user choose the next operation
         choice = ''
