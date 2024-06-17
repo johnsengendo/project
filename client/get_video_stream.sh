68d4aa91ba56ef9bb1df00c3e1a086e08976122c
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-17 17:27:56 +0200

    Update get_video_stream.sh

--- client/get_video_stream.sh
+++ client/get_video_stream.sh
@@ -40,7 +40,8 @@ fi
 
 # Get the video stream and save it to the specified file
 start_time=$(date +%s)
-ffmpeg -i rtmp://10.0.0.1:1935/live/video.flv -t 5 -probesize 80000 -analyzeduration 15 -c:a copy -c:v copy "${out_file}"
+#ffmpeg -i rtmp://10.0.0.1:1935/live/video.flv -t 5 -probesize 80000 -analyzeduration 15 -c:a copy -c:v copy "${out_file}"
+ffmpeg -loglevel info -stats -i rtmp://10.0.0.1:1935/live/video.flv -t 5 -probesize 80000 -analyzeduration 15 -c:a copy -c:v copy "${out_file}"
 
 run_time=$(($(date +%s) - start_time))
 
