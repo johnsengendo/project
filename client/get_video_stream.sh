d4c4af9fa2c53251285c34fd16213d732a25e846
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-17 15:14:02 +0200

    Add files via upload

---
+++ client/get_video_stream.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Function to print usage on terminal
+function usage {
+    echo "Usage: ./get_streaming.sh [-o stream_output_filename] [-d]"
+    echo "    -o stream_output_filename = name of the (.flv) output file without extension (the default value
+                                          is 'stream_output')"
+    echo "    -d = disable the capture of the video stream incoming packets through tcpdump (the capture is
+                   enabled by default, with output file 'shared/client_out.pcap')"
+}
+
+# Parse the script arguments
+out_file="stream_output.flv"
+capture_traffic="true"
+while getopts 'o:dh' OPTION; do
+    case "$OPTION" in
+        o)
+            out_file="${OPTARG}.flv"
+            ;;
+        d)
+            capture_traffic="false"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 0
+            ;;
+    esac
+done
+shift "$((OPTIND -1))"
+
+# Start tcpdump capture, if required
+if [ ${capture_traffic} == "true" ]; then
+    ./tcpdump_utils/start_capture_client.sh
+    sleep 2
+fi
+
+# Get the video stream and save it to the specified file for 5 seconds using GStreamer
+start_time=$(date +%s)
+gst-launch-1.0 rtmpsrc location="rtmp://10.0.0.1:1935/live/video.flv" ! flvdemux ! decodebin ! videoconvert ! x264enc ! flvmux ! filesink location="${out_file}" -e -t 5
+
+run_time=$(($(date +%s) - start_time))
+
+# Stop tcpdump capture, if required
+if [ ${capture_traffic} == "true" ]; then
+    ./tcpdump_utils/stop_capture.sh
+    sleep 3
+fi
+
+# Print the stream acquisition run time
+printf "\n"
+echo The stream acquisition run time is ${run_time}s
