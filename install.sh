a150198d2572d2629c3f2df8d0fd8c611344f2aa
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-17 15:22:09 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav
