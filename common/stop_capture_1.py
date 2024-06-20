f6e8d22034b231ea7ebf7bf7496adfcd9c0d931d
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-20 13:03:14 +0200

    Add files via upload

---
+++ common/stop_capture_1.py
@@ -0,0 +1,17 @@
+
+import subprocess
+import os
+import signal
+
+def stop_capture():
+    # Get the PID of the tcpdump process
+    ps_output = subprocess.check_output(['ps', '-e']).decode('utf-8')
+    for line in ps_output.splitlines():
+        if 'tcpdump' in line:
+            pid = int(line.split()[0])
+            # Send SIGINT (signal number 2 in bash)
+            os.kill(pid, signal.SIGINT)
+            break
+
+if __name__ == "__main__":
+    stop_capture()
