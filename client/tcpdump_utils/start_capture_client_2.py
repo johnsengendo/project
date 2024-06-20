bc7c0b4c693888169985e39cb14a799ef045541d
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-20 13:01:48 +0200

    Add files via upload

---
+++ client/tcpdump_utils/start_capture_client_2.py
@@ -0,0 +1,8 @@
+
+import subprocess
+
+def start_capture():
+    subprocess.Popen(["tcpdump", "-U", "-s0", "-i", "client-eth0", "src", "port", "1935", "-w", "pcap/client_out.pcap"])
+
+if __name__ == "__main__":
+    start_capture()
