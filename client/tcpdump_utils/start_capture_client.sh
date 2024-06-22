88f49f78e87abd52d2421b4aa4e0aa110d485ae7
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-22 13:06:37 +0200

    Delete client/tcpdump_utils directory

--- client/tcpdump_utils/start_capture_client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start tcpdump in background in order to capture the video streaming traffic coming from the server
-tcpdump -U -s0 -i client-eth0 src port 1935 -w pcap/client_out.pcap &
