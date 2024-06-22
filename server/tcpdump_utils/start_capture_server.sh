f3dc0a5c638bbae04c2fb70a2dd9cfcbc2b82641
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-22 13:12:17 +0200

    Delete server/tcpdump_utils directory

--- server/tcpdump_utils/start_capture_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash/python3
-
-# Start tcpdump in background in order to capture the video streaming traffic outgoing from the server
-tcpdump -U -s0 -i server-eth0 src port 1935 -w pcap/server_out.pcap &
