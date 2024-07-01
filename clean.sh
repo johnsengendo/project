d9e01aff4751397c802b2ab4cc8d8d0863a16e5b
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-07-01 12:57:16 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# Cleaning up Mininet
+echo "Cleaning up Mininet..."
 sudo mn -c
+
+# Stopping all Docker containers
+echo "Stopping all running Docker containers..."
 docker stop $(docker ps -aq)
+
+# Removing all stopped Docker containers
+echo "Removing all stopped Docker containers..."
 docker container prune -f
+
+echo "Cleanup complete."
