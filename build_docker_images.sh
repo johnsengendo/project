ff63034ac9144e8aa51d1c7ca7a732947b8c35bc
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-07-01 12:09:49 +0200

    Update build_docker_images.sh

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Building docker image for server streaming"
-docker build -t streaming_server_image --file ./server/Dockerfile.server ./server
+# Defining an associative array with key-value pairs
+# keys are the roles and the values are the directories
+declare -A build_configs=(
+    ["streaming_server_image"]="server"
+    ["streaming_client_image"]="client"
+)
 
-echo "Building docker image for client streaming"
-docker build -t streaming_client_image --file ./client/Dockerfile.client ./client
+# Looping through the associative array
+for image_tag in "${!build_configs[@]}"; do
+    echo "Building docker image for ${build_configs[$image_tag]} streaming"
+    docker build -t "$image_tag" --file "./${build_configs[$image_tag]}/Dockerfile.${build_configs[$image_tag]}" "./${build_configs[$image_tag]}"
+done
