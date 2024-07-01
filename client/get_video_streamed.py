8a66d40dc0500aa14696ef95f03410b5d5512d5b
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-07-01 12:52:22 +0200

    Update get_video_streamed.py

--- client/get_video_streamed.py
+++ client/get_video_streamed.py
@@ -34,7 +34,6 @@ def get_video_stream():
         pid = start_capture() # Starting to capture traffic
         time.sleep(2)
 
-    start_time = time.time()
     ffmpeg_command = [
         "ffmpeg", "-loglevel", "info", "-stats", "-i", "rtmp://10.0.0.1:1935/live/video.flv",
         "-t", "10", "-probesize", "80000", "-analyzeduration", "15", "-c:a", "copy", "-c:v", "copy", out_file
