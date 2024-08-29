4aacafbc657eeff1409ca8886efef3dccf57774b
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-08-29 07:05:59 +0200

    Update network-topology-script.py

--- network-topology-script.py
+++ network-topology-script.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import argparse
 import os
 import subprocess
 import sys
 import time
 import threading
-
 from comnetsemu.cli import CLI, spawnXtermDocker
 from comnetsemu.net import Containernet, VNFManager
 from mininet.link import TCLink
@@ -29,25 +27,23 @@ def start_server():
 def start_client():
     subprocess.run(['docker', 'exec', '-it', 'streaming_client', 'bash', '-c', 'cd /home && python3 get_video_streamed.py'])
 
-def change_link_properties(link, bw, delay):
-    info(f'*** Changing bandwidth to {bw} Mbps and delay to {delay} ms\n')
-    link.intf1.config(bw=bw, delay=f'{delay}ms')
-    link.intf2.config(bw=bw, delay=f'{delay}ms')
+def change_bandwidth(link, new_bw):
+    info(f'*** Changing bandwidth to {new_bw} Mbps\n')
+    link.intf1.config(bw=new_bw)
+    link.intf2.config(bw=new_bw)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Video streaming application with dynamic bandwidth and delay.')
+    parser = argparse.ArgumentParser(description='Video streaming application with dynamic bandwidth.')
     parser.add_argument('--autotest', dest='autotest', action='store_true', default=False,
                         help='Enables automatic testing of the topology and closes the streaming application.')
     args = parser.parse_args()
 
-    # Define 15 bandwidth-delay pairs
-    bw_delay_pairs = [
-        (5, 10), (10, 20), (15, 30), (20, 40), (25, 50),
-        (30, 60), (35, 70), (40, 80), (45, 90), (50, 100),
-        (55, 110), (60, 120), (65, 130), (70, 140), (75, 150)
-    ]
-    
+    initial_bandwidth = 10  # initial bandwidth in Mbps
+    delay = 5       # delay in milliseconds
     autotest = args.autotest
+
+    # Define 15 bandwidth values
+    bandwidth_values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
 
     script_directory = os.path.abspath(os.path.dirname(__file__))
     shared_directory = os.path.join(script_directory, 'pcap')
@@ -71,7 +67,7 @@ if __name__ == '__main__':
     switch1 = net.addSwitch('s1')
     switch2 = net.addSwitch('s2')
     net.addLink(switch1, server)
-    middle_link = net.addLink(switch1, switch2, bw=bw_delay_pairs[0][0], delay=f'{bw_delay_pairs[0][1]}ms')
+    middle_link = net.addLink(switch1, switch2, bw=initial_bandwidth, delay=f'{delay}ms')
     net.addLink(switch2, client)
 
     info('\n*** Starting network\n')
@@ -91,10 +87,10 @@ if __name__ == '__main__':
     time.sleep(2)  # Give the server time to start
     client_thread.start()
 
-    # Change link properties every 120 seconds
-    for bw, delay in bw_delay_pairs:
-        change_link_properties(middle_link, bw, delay)
-        time.sleep(120)
+    # Change bandwidth 15 times
+    for bw in bandwidth_values[1:]:  # Start from the second value as the first is already set
+        time.sleep(120)  # Wait for 120 seconds before changing bandwidth
+        change_bandwidth(middle_link, bw)
 
     # Wait for threads to finish
     server_thread.join()
