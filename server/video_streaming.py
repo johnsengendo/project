93789a9f2d0c6f078bebb313c10e01ce9c5eca3c
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-12-20 15:07:59 +0100

    Update video_streaming.py

--- server/video_streaming.py
+++ server/video_streaming.py
@@ -13,22 +13,30 @@ def start_capture():
     on source port 1935 and writing them to a pcap file. This is used to
     capture network traffic associated with the video streaming.
     """
-    #proc = subprocess.Popen(["tcpdump", "-U", "-s0", "-i", "server-eth0", "src", "port", "1935", "-w", "pcap/server.pcap"])
-    proc = subprocess.Popen(["tcpdump", "-U", "-s0", "-i", "h6-eth0", "src", "port", "1935", "-w", "pcap/server.pcap", "-A"])
+    proc = subprocess.Popen(["tcpdump", "-U", "-s0", "-i", "server-eth0", "src", "port", "1935", "-w", "pcap/server.pcap"])
+    #proc = subprocess.Popen(["tcpdump", "-U", "-s0", "-i", "h6-eth0", "src", "port", "1935", "-w", "pcap/server.pcap", "-A"])
 
     return proc.pid
 
-def stop_capture(pid):
+def start_capture_h6():
+    """
+    Start packet capturing on h6-eth0 interface using tcpdump.
+    Appends captured packets to the server.pcap file.
+    """
+    proc = subprocess.Popen(["tcpdump", "-U", "-s0", "-i", "h6-eth0", "src", "port", "1935", "-w", "pcap/server.pcap", "-A"])
+    return proc.pid
+
+def stop_capture(pid_list):
     """
     Stoping the tcpdump process that was started by start_capture. Searches through
     the process list, identifies the tcpdump process, and sends a SIGINT signal to
     terminate it.
     """
-    try:
-        os.kill(pid, signal.SIGINT)
-        print("Capture stopped successfully.")
-    except OSError as e:
-        print(f"Error stopping capture: {e}")
+    for pid in pid_list:
+        try:
+            os.kill(pid, signal.SIGINT)
+        except OSError as e:
+            print(f"Error stopping capture: {e}")    
 
 def main():
     """
@@ -38,8 +46,12 @@ def main():
     loops_number = 0  # Stream the video once, without looping
     capture_traffic = True
 
+    pids = []
+
     if capture_traffic:
-        pid = start_capture()  # Begining packet capturing
+        pids.append(start_capture())
+        pids.append(start_capture_h6())
+        #pid = start_capture()  # Begining packet capturing
         time.sleep(2)   # Short delay to ensure capturing starts before streaming
 
     ffmpeg_command = [
@@ -50,7 +62,7 @@ def main():
     subprocess.run(ffmpeg_command)  # Running ffmpeg command to stream video
 
     if capture_traffic:
-        stop_capture(pid)  # Stopping packet capturing after streaming is done
+        stop_capture(pids)  # Stopping packet capturing after streaming is done
 
 if __name__ == "__main__":
     main()
